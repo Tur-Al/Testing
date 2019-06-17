36287621b5e5d953f1d35b1622ec2532b00524cd
Tural <turalkerimovtural@mail.ru>
2019-06-17 14:42:46 +0500

    19-й

--- lesson 14 - Кон. валют через  Promise/Конвертер валют/js/script14-2.js
+++ lesson 14 - Кон. валют через  Promise/Конвертер валют/js/script14-2.js
@@ -1,26 +1,26 @@
 // Задание 14-2
 
-let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
+let inputRub = document.getElementById('rub'),      // получаем "id" поля для ввода рублей
+    inputUsd = document.getElementById('usd');      // получаем "id" поля для ввода долларов
 
-inputRub.addEventListener('input', () => {
+inputRub.addEventListener('input', () => {      // назначаем событие на поле для ввода рублей
 
-    function catchData() {
+    function catchData() {      
 
-        return new Promise(function(resolve, reject) {
-            let request = new XMLHttpRequest();
-            request.open("GET", "js/current.json");
+        return new Promise(function(resolve, reject) {      // возвращаем новый промис
+            let request = new XMLHttpRequest();     // новый запрос
+            request.open("GET", "js/current.json");     // метод и url сервера
         
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            request.send();
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');    // заголовок запроса
+            request.send();     // отправляем данные на сервер
         
-            request.onload = function() {
-                if(request.readyState === 4) {
-                    if(request.status == 200) {
-                        resolve(this.response)
+            request.onload = function() {   
+                if(request.readyState === 4) {      // проверка на состояние запроса
+                    if(request.status == 200) {     // проверка на состояние запроса
+                        resolve(this.response)      // если проверка пройдена - получить ответ сервера
                     }
                     else {
-                        reject();     
+                        reject();     // в противном случае ошибка
                     }
                 }
             }
@@ -30,9 +30,9 @@ inputRub.addEventListener('input', () => {
     catchData()
     .then(response => {
         console.log(response);
-        let data = JSON.parse(response);
-        inputUsd.value = inputRub.value / data.usd;
+        let data = JSON.parse(response);    // в переменную "data" помещаем распарсированный ответ сервера
+        inputUsd.value = inputRub.value / data.usd;     // в поле ввода долларов выводим сконвертированный результат
     })
     .then(() => console.log(5000))
-    .catch(() => inputUsd.value = "Что-то пошло не так")
+    .catch(() => inputUsd.value = "Что-то пошло не так")    // в случае ошибки в поле ввода долларов выводим сообщение
 });
